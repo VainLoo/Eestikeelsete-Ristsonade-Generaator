b0662fda5f78d3b76648dea74c8c9fd308171614
VainLoo <loo_allan@yahoo,com>
2022-03-12 14:32:35 +0200

    Job timeout time reduction

--- backend/server/main/jobs.py
+++ backend/server/main/jobs.py
@@ -6,12 +6,7 @@ from rq.decorators import job
 from rq import Retry
 import logging
 
-# the timeout parameter specifies how long a job may take
-# to execute before it is aborted and regardes as failed
-# the result_ttl parameter specifies how long (in seconds)
-# successful jobs and their results are kept.
-# for more detail: https://python-rq.org/docs/jobs/
-@job('default', connection=redis_connection, timeout=120, result_ttl=-1, retry=Retry(max=3))
+@job('default', connection=redis_connection, timeout=90, result_ttl=-1, retry=Retry(max=3))
 def crossword(width, length):
     grid, words = getCrossword(length, width)
     logging.info("Crossword generated")
